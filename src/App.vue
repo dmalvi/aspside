97cefe635521dce08bd4e26f96f71c1f42bafdb1
dmalvi <dmalvi@gmail.com>
2024-02-05 14:50:26 +0100

    initial

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+</script>
+
+<template>
+  <main>
+    <div class="message">
+      <p>This site moved to: </p>
+      <a href="https://aspside-next.vercel.app/">https://aspside-next.vercel.app/</a>
+    </div>
+  </main>
+</template>
+
+<style scoped>
+.message {
+  font-size: larger;
+}
+p {
+ font-weight: bold; 
+}
+</style>
